c91209da8fa38aa2c31ca59f2572e95559e245d5
wonSage <34784300+wonsage@users.noreply.github.com>
2022-03-31 22:22:54 +0800

    移动端 rem 适配

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
 
 import Vant from 'vant' // 引入 Vant
 import 'vant/lib/index.css'
