c9e89b1df70516a18a5b65a46ad21382c25577f0
wonSage <34784300+wonsage@users.noreply.github.com>
2022-03-31 21:37:48 +0800

    customize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant' // 引入 Vant
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
